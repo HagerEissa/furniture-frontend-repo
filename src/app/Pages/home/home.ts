70c7184134ba189bdba927bcf50c4e1e2c37f65d
Roaa Mounir <your-email@example.com>
2025-10-19 21:52:55 +0300

    Add hero section with styling

--- src/app/Pages/home/home.ts
+++ src/app/Pages/home/home.ts
@@ -1,20 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Navbar } from '../../components/navbar/navbar';
-import { Categories } from "../../components/categories/categories";
-import { Products } from "../../components/products/products";
-import { Homegrid } from "../../components/homegrid/homegrid";
+import { Categories } from '../../components/categories/categories';
+import { Products } from '../../components/products/products';
+import { Homegrid } from '../../components/homegrid/homegrid';
+import { HeroHome } from '../../components/hero-home/hero-home';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, Navbar, Categories, Products, Homegrid],
+  imports: [RouterModule, Navbar, Categories, Products, Homegrid, HeroHome],
   templateUrl: './home.html',
   styleUrls: ['./home.css'],
 })
 export class Home {
   // showMenu = false;
-
   // toggleMenu() {
   //   this.showMenu = !this.showMenu;
   // }
