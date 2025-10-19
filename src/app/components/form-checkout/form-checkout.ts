13854842969023ce1e62b23f29b0cf1cb0007fc9
Roaa Mounir <your-email@example.com>
2025-10-19 23:42:29 +0300

    Add Checkout Page

---
+++ src/app/components/form-checkout/form-checkout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-checkout',
+  imports: [],
+  templateUrl: './form-checkout.html',
+  styleUrl: './form-checkout.css'
+})
+export class FormCheckout {
+
+}
