1d8fdef72f2037e8894d523bddd6ca8529c02a97
Roaa Mounir <your-email@example.com>
2025-10-19 22:48:16 +0300

    Add Hero-Shop with styling

--- src/app/Pages/shop/shop.ts
+++ src/app/Pages/shop/shop.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Banner } from "../../components/banner/banner";
+import { HeroShop } from '../../components/hero-shop/hero-shop';
 
 @Component({
   selector: 'app-shop',
-  imports: [Banner],
+  imports: [Banner,HeroShop],
   templateUrl: './shop.html',
   styleUrl: './shop.css'
 })
