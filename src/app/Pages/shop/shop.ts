e563abf19d012f84f957ce8e0609244ea3457e43
Salma Fadl <salmafadlabdulrahman@gmail.com>
2025-10-20 09:43:37 +0300

    added stylings and prouducts component

--- src/app/Pages/shop/shop.ts
+++ src/app/Pages/shop/shop.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Banner } from "../../components/banner/banner";
 import { HeroShop } from '../../components/hero-shop/hero-shop';
 import { ControlBar } from '../../components/control-bar/control-bar';
+import { Products } from "../../components/products/products";
 
 @Component({
   selector: 'app-shop',
-  imports: [Banner,HeroShop,ControlBar],
+  imports: [Banner, HeroShop, ControlBar, Products],
   templateUrl: './shop.html',
   styleUrl: './shop.css'
 })
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Login } from "./components/login/login";
-import { Signup } from "./components/signup/signup";
-import { SignupWithMail } from "./components/signup-with-mail/signup-with-mail";
-import { Home } from './Pages/home/home';
-import { Homegrid } from './components/homegrid/homegrid';
-import { Categories } from "./components/categories/categories";
-import { Products } from "./components/products/products";
 import { Footer } from './components/footer/footer';
 import { Navbar } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login, Signup, SignupWithMail, Home, Homegrid, Categories, Products,Footer,Navbar],
+  imports: [RouterOutlet, Footer, Navbar],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('frontend');
