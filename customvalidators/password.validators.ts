d96751197d2257d3316b4800083b613cf1765dd7
Roaa Mounir <your-email@example.com>
2025-10-21 19:40:28 +0300

    edit routing ,edit styling

--- customvalidators/password.validators.ts
+++ customvalidators/password.validators.ts
@@ -1,22 +1,21 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
 
-export class PasswordValidor{
-
-static passwordStrength():ValidatorFn{
-    return (control:AbstractControl) : ValidationErrors | null=>{
+export class PasswordValidator {
+  static passwordStrength(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
       const value = control.value;
-      if(!value){
+      if (!value) {
         return null;
       }
+
       const hasNumber = /[0-9]/.test(value);
       const hasUpper = /[A-Z]/.test(value);
       const hasLower = /[a-z]/.test(value);
       const isValidLength = value.length >= 8;
+
       const passwordValid = hasNumber && hasLower && hasUpper && isValidLength;
 
-      return passwordValid ? null : {passwordstrength:true};
-
-    }
-}
-
+      return passwordValid ? null : { passwordstrength: true };
+    };
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,44 @@
+// import { Routes } from '@angular/router';
+// import { Home } from '../app/Pages/home/home';
+// import { Shop } from '../app/Pages/shop/shop';
+// import { About } from '../app/Pages/about/about';
+// import { Contact } from '../app/Pages/contact/contact';
+// import { Checkout } from './Pages/checkout/checkout';
+// import { Login } from './components/login/login';
+// import { Signup } from './components/signup/signup';
+
+// export const routes: Routes = [
+//   { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+//   { path: 'login', component: Login },
+//   { path: 'signup', component: Signup },
+
+//   { path: 'home', component: Home },
+//   { path: 'shop', component: Shop },
+//   { path: 'about', component: About },
+//   { path: 'contact', component: Contact },
+//   { path: 'checkout', component: Checkout },
+
+//   // لو دخل مسار غلط
+//   { path: '**', redirectTo: 'login' },
+// ];
 import { Routes } from '@angular/router';
 import { Home } from '../app/Pages/home/home';
-
 import { Shop } from '../app/Pages/shop/shop';
 import { About } from '../app/Pages/about/about';
 import { Contact } from '../app/Pages/contact/contact';
 import { Checkout } from './Pages/checkout/checkout';
+import { Login } from './components/login/login';
+import { Signup } from './components/signup/signup';
 
 export const routes: Routes = [
-  { path: '', component: Home },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'signup', component: Signup },
+  { path: 'home', component: Home },
   { path: 'shop', component: Shop },
   { path: 'about', component: About },
   { path: 'contact', component: Contact },
-    { path: 'Checkout', component: Checkout },
-
+  { path: 'checkout', component: Checkout },
+  { path: '**', redirectTo: 'login' },
 ];
