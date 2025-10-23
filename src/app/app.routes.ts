755a8951b214142381bed036f12a0e49eebeeb96
Roaa Mounir <your-email@example.com>
2025-10-23 14:47:21 +0300

    resolve merge conflicts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,27 +1,4 @@
-// import { Routes } from '@angular/router';
-// import { Home } from '../app/Pages/home/home';
-// import { Shop } from '../app/Pages/shop/shop';
-// import { About } from '../app/Pages/about/about';
-// import { Contact } from '../app/Pages/contact/contact';
-// import { Checkout } from './Pages/checkout/checkout';
-// import { Login } from './components/login/login';
-// import { Signup } from './components/signup/signup';
 
-// export const routes: Routes = [
-//   { path: '', redirectTo: 'login', pathMatch: 'full' },
-
-//   { path: 'login', component: Login },
-//   { path: 'signup', component: Signup },
-
-//   { path: 'home', component: Home },
-//   { path: 'shop', component: Shop },
-//   { path: 'about', component: About },
-//   { path: 'contact', component: Contact },
-//   { path: 'checkout', component: Checkout },
-
-//   // لو دخل مسار غلط
-//   { path: '**', redirectTo: 'login' },
-// ];
 import { Routes } from '@angular/router';
 import { Home } from '../app/Pages/home/home';
 import { Shop } from '../app/Pages/shop/shop';
@@ -32,10 +9,10 @@ import { Dashboard } from './Pages/dashboard/dashboard';
 import { Profile } from './Pages/profile/profile';
 import { Login } from './components/login/login';
 import { Signup } from './components/signup/signup';
-import { NotFound } from './components/not-found/not-found';
+import { OauthSuccess } from './components/oauth-success/oauth-success';
 import { Cart } from './components/cart/cart';
 import { Favourite } from './components/favourite/favourite';
-import { OauthSuccess } from './components/oauth-success/oauth-success';
+import { NotFound } from './components/not-found/not-found';
 
 
 export const routes: Routes = [
