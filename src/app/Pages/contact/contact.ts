82add5e90a776b957b5166f8811b53a6db63fdf0
SandyAzzat <sandy.azzat.youssef@gmail.com>
2025-10-23 15:56:22 +0300

    Edit contact page and contact-layout components

--- src/app/Pages/contact/contact.ts
+++ src/app/Pages/contact/contact.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroContact } from '../../components/hero-contact/hero-contact';
+import { ContactLayout } from "../../components/contact-layout/contact-layout";
+import { Banner } from "../../components/banner/banner";
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [HeroContact],
+  imports: [HeroContact, ContactLayout, Banner],
   templateUrl: './contact.html',
   styleUrls: ['./contact.css'],
 })
