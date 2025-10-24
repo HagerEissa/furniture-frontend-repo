ba6e18006e253be16b1e05ace10e1174308d67a6
Roaa Mounir <your-email@example.com>
2025-10-24 17:56:25 +0300

    editNav

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -2,7 +2,6 @@ import { RouterModule, Router } from '@angular/router';
 import { AuthStateService } from '../../core/services/auth-state.service';
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { FavouriteService } from '../../core/services/favourite-service';
 import { Auth } from '../../core/services/auth';
 
@@ -17,11 +16,11 @@ export class Navbar implements OnInit {
   showMenu = false;
   isLoggedIn = false;
 
-  constructor(private authState: AuthStateService, private auth: Auth, private router: Router) {
+  constructor(private authState: AuthStateService, private auth: Auth, private router: Router,private _favouriteService:FavouriteService,private _authService:Auth) {
     this.authState.isLoggedIn$.subscribe((status) => (this.isLoggedIn = status));
   }
   favCount: number = 0;
-  constructor(private _favouriteService:FavouriteService,private _authService:Auth) {}
+  // constructor(private _favouriteService:FavouriteService,private _authService:Auth) {}
   ngOnInit(): void {
     let userId:string=this._authService.getUserId();
     if (!userId) return;
