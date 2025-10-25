710465bb9ed9c01d7a41c2f5e9729770a61b4e6c
Roaa Mounir <your-email@example.com>
2025-10-25 20:02:47 +0300

    edit order

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,24 +13,34 @@ import { Favourite } from './components/favourite/favourite';
 import { NotFound } from './components/not-found/not-found';
 import { Blog } from './Pages/blog/blog';
 import { ProductDetail } from './components/product-detail/product-detail';
+import { PaymentSuccessComponent } from './components/suceesspayment/suceesspayment';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
   { path: 'signup', component: Signup },
   { path: 'oauth-success', component: OauthSuccess },
-  { path: 'home', component: Home ,title:'Home'},
-  { path: 'shop', component: Shop ,title:'Shop' },
-  {path:'about',loadComponent:()=>import('../app/Pages/about/about').then(c=>c.About),title:'About'}, //lazyload
-  {path:'contact',loadComponent:()=>import('../app/Pages/contact/contact').then(c=>c.Contact),title:'contact'}, //lazyload
-  { path: 'Checkout', component: Checkout ,title:'Checkout'},
-  { path: 'dashboard', component: Dashboard ,title:'dashboard'},
-  { path: 'profile', component: Profile ,title:'profile'},
-  { path: 'cart', component: Cart ,title:'Cart'},
-  { path: 'favourite', component: Favourite ,title:'Favourite'},
+  { path: 'home', component: Home, title: 'Home' },
+  { path: 'shop', component: Shop, title: 'Shop' },
+  {
+    path: 'about',
+    loadComponent: () => import('../app/Pages/about/about').then((c) => c.About),
+    title: 'About',
+  }, //lazyload
+  {
+    path: 'contact',
+    loadComponent: () => import('../app/Pages/contact/contact').then((c) => c.Contact),
+    title: 'contact',
+  }, //lazyload
+  { path: 'Checkout', component: Checkout, title: 'Checkout' },
+  { path: 'dashboard', component: Dashboard, title: 'dashboard' },
+  { path: 'profile', component: Profile, title: 'profile' },
+  { path: 'cart', component: Cart, title: 'Cart' },
+  { path: 'favourite', component: Favourite, title: 'Favourite' },
   // { path: 'oauthsuccesscomponent', component: OauthSuccessComponent },
-  { path: 'blog', component: Blog ,title:'Blog'},
-  { path: 'product-detail/:id', component: ProductDetail ,title:'ProductDetail'},
+  { path: 'blog', component: Blog, title: 'Blog' },
+  { path: 'product-detail/:id', component: ProductDetail, title: 'ProductDetail' },
+  { path: 'payment-success', component: PaymentSuccessComponent, title: 'Payment Success' },
 
-  { path: '**', component: NotFound ,title:'NotFound'},
+  { path: '**', component: NotFound, title: 'NotFound' },
 ];
