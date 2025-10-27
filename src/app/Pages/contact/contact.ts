d9bc83003a082195a6d7b3983ae54840693dcc51
Roaa Mounir <your-email@example.com>
2025-10-27 17:26:30 +0300

    edit contant

--- src/app/Pages/contact/contact.ts
+++ src/app/Pages/contact/contact.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeroContact } from '../../components/hero-contact/hero-contact';
 import { ContactLayout } from "../../components/contact-layout/contact-layout";
 import { Banner } from "../../components/banner/banner";
+import { Auth } from '../../core/services/auth';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [HeroContact, ContactLayout, Banner],
+  imports: [HeroContact, ContactLayout, Banner, CommonModule],
   templateUrl: './contact.html',
   styleUrls: ['./contact.css'],
 })
-export class Contact {}
+export class Contact implements OnInit {
+  isAdminUser = false;
+
+  constructor(private _authService: Auth) {}
+
+  ngOnInit(): void {
+    this.isAdminUser = this._authService.isAdmin();
+  }
+}
